d4d7efe471a73359690bbc1ae4aa17e2a936530f
221B179 <221B179@juet.co.in>
2025-02-04 11:40:26 +0530

    lab02prob2

---
+++ LAB02/PROB2/Voice2.java
@@ -0,0 +1,14 @@
+class Voice2 {
+
+    private Voice voice;
+
+    public Voice2() {
+        voice = new Voice();
+    }
+
+    
+    public void templateMethod() {
+        voice.prepareVoice(); 
+        voice.hear();        
+    }
+}
